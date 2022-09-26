399fee478b24513dd2d393c3a46cd7176839d074
itsaln <itsaln23@gmail.com>
2022-09-26 18:14:15 +0500

    02

--- 02-Top-Restaurants/components/restaurant/Row.vue
+++ 02-Top-Restaurants/components/restaurant/Row.vue
@@ -9,7 +9,10 @@
 		:style='index % 2 === 0 ? {backgroundColor: "rgba(128, 128, 128, 0.15)"} : null'
 	>
 		<h4 class='header rank'>{{ rank }}</h4>
-		<a href='`restaurants/${name}`' class='header link'>{{ name }}</a>
+		<nuxt-link
+			:href='`restaurants/${name}`'
+			class='header link'
+		>{{ name }}</nuxt-link>
 	</div>
 </template>
 
