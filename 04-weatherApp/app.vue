ebbc69c724b6f7a488f8293829e0e45d2d208080
itsaln <itsaln23@gmail.com>
2022-10-07 17:23:31 +0500

    04

--- 04-weatherApp/app.vue
+++ 04-weatherApp/app.vue
@@ -26,24 +26,35 @@
 </template>
 
 <script setup lang='ts'>
-const search = ref('Tashkent')
+const cookie = useCookie('city')
+
+if (!cookie.value) cookie.value = 'New York'
+
+const search = ref(cookie.value)
 const input = ref('')
 const background = ref('')
 
 const { data: city, error } = useAsyncData('city', async () => {
-	const response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=bda6a2479c3165876fa7335f499d08c1`)
+	let response
 
-	const temp = response.main.temp
+	try {
+		response = await $fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=bda6a2479c3165876fa7335f499d08c1`
+		)
 
-	if (temp <= -10) {
-		background.value = 'https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80'
-	} else if (temp > -10 && temp <= 0) {
-		background.value = 'https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80'
-	} else if (temp > 0 && temp <= 10) {
-		background.value = 'https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80'
-	} else {
-		background.value = 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80'
-	}
+		cookie.value = search.value
+		const temp = response.main.temp
+
+		if (temp <= -10) {
+			background.value = 'https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80'
+		} else if (temp > -10 && temp <= 0) {
+			background.value = 'https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80'
+		} else if (temp > 0 && temp <= 10) {
+			background.value = 'https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80'
+		} else {
+			background.value = 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80'
+		}
+	} catch (e) {}
 
 	return response
 }, {
